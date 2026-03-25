8203fdea33a860879eccbbb7040789119603b302
aapelix <aaro@tuta.com>
2026-03-25 16:48:18 +0200

    cookieless mode

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -6,6 +6,7 @@ import posthog from "posthog-js";
 posthog.init("phc_9a8liLr4O60FcK3SImNVTJm62uf04gvQ9LPej4wgJVm", {
   api_host: "https://j.aapelix.dev",
   defaults: "2026-01-30",
+  cookieless_mode: "always",
 });
 
 export default mount(() => <StartClient />, document.getElementById("app")!);
